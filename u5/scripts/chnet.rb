375ed93b23893ebf20f38fa10d589520a8bde533
jorge chinea polegre <jorgheavy93@gmail.com>
2019-12-17 08:38:03 +0000

    U

--- u5/scripts/chnet.rb
+++ u5/scripts/chnet.rb
@@ -9,12 +9,12 @@ def menu
   option = gets.chop
 
  if option == 'r'
-    system (ifdown eth0)
-    sysetm (ifup eth0)
+    system ("ifdown eth0")
+    sysetm ("ifup eth0")
   if option == '1'
-    system (ip addr add 172.19.11.33/16 dev eth0)
+    system ("ip addr add 172.19.11.33/16 dev eth0")
   if option == '2'
-    system (ip addr add 192.168.1.50/24 dev eth0)
+    system ("ip addr add 192.168.1.50/24 dev eth0")
 end
 
 menu
